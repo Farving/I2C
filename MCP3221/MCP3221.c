5af35d201cfe9b34f8d8008fa7baf056a2b9803a
Carsten Farving <farving@gmail.com>
2014-02-15 23:53:08 +0100

    Initial stage

---
+++ MCP3221/MCP3221.c
@@ -0,0 +1,35 @@
+/** INCLUDES ************************************************************************************/
+#include <xc.h>
+
+#include <stdio.h>
+#include <stdbool.h>       // Includes true/false definition
+
+#include "../Includes/bb_i2c/bb_i2c.h"
+#include "MCP3221.h"
+
+/** Functions ************************************************************************************/
+unsigned char MCP3221_Device_Connection_Test()
+{
+	I2C_Start();
+	return I2C_Tx(MCP3221_ADDR << 1 | I2C_WRITE);
+}
+
+unsigned char MCP3221_Initialize()
+{
+	I2C_Init();
+	return MCP3221_Device_Connection_Test();
+}
+
+unsigned int MCP3221_Read_Conversion()
+{
+	unsigned int data = 0;
+
+	I2C_Start();
+	I2C_Tx(MCP3221_ADDR << 1 | I2C_READ);
+	data = I2C_Rx(I2C_ACK) & 0x0F;
+        data <<= 8;
+	data |= I2C_Rx(I2C_NACK);
+	I2C_Stop();
+
+        return data;
+}
